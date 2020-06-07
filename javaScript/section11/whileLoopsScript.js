c7b397de79b481a06365d11dba6c3b7b57f512df
sb62 <kjr27@psu.edu>
2020-06-06 22:53:48 -0400

    Cleanup and adding while loop practice

---
+++ javaScript/section11/whileLoopsScript.js
@@ -0,0 +1,15 @@
+console.log("Print all numbers between -10 and 19")
+
+let num = -10
+while (num >= -10 && num <= 19) {
+    console.log(num);
+    num++;
+}
+
+console.log("Print all even numbers between 10 and 40")
+
+let numTwo = 10
+while (numTwo >= 10 && numTwo <= 40) {
+    console.log(numTwo);
+    numTwo += 2;
+}
