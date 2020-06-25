4e41de9259c2601d346dce037d27bd55c78c563e
sb62 <kjr27@psu.edu>
2020-06-24 22:45:26 -0400

    added reset button functionality"

--- javaScript/section17/colorGameScript.js
+++ javaScript/section17/colorGameScript.js
@@ -5,6 +5,21 @@ let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
 let h1 = document.querySelector("h1");
+let resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function () {
+  // generate all new colors
+  colors = generateRandomColors(6);
+  // pick a new random color as solution
+  pickedColor = pickColor();
+  // chagne colorDisplay to match picked color
+  colorDisplay.textContent = pickColor();
+  // change colors on page
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+  }
+  h1.style.backgroundColor = "#232323";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -19,6 +34,7 @@ for (let i = 0; i < squares.length; i++) {
     //   Compare Color to pickedColor
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct!";
+      resetButton.textContent = "Play Again?";
       changeColors(clickedColor);
       h1.style.backgroundColor = clickedColor;
     } else {
