ad205839dae0d84268cdff5c0e07e6cfd46156f1
sb62 <skullbasher62@gmail.com>
2020-07-08 17:59:31 -0400

    functionally complete

--- javaScript/section20/assets/js/todoScript.js
+++ javaScript/section20/assets/js/todoScript.js
@@ -1,14 +1,26 @@
 // Checkoff specific todos by clicking - toggles done class defined in the CSS
-$("li").on("click", function () {
+$("ul").on("click", "li", function () {
   $(this).toggleClass("done");
 });
 
 // Delete Todos
-$(".deleteButton").on("click", function (event) {
+$("ul").on("click", ".deleteButton", function (event) {
   $(this)
     .parent()
     .fadeOut("500", function () {
       $(this).remove();
     });
   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event) {
+  if (event.which === 13) {
+    // extract typed value and clear input
+    let todoText = $(this).val();
+    $(this).val("");
+    // create new li and add to ul
+    $("ul").append(
+      "<li><span class='deleteButton'>X</span> " + todoText + "</li>"
+    );
+  }
 });
